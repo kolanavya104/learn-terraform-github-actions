3cee8795b75518c0be360591e95ce8fe0357fd97
Navya <navyakola104@gmail.com>
2024-09-26 11:43:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,9 +41,6 @@ resource "aws_instance" "web" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
   vpc_security_group_ids = [aws_security_group.web-sg.id]
-  tags = {
-    Name = "Git"
-  }
 
 
   user_data = <<-EOF
